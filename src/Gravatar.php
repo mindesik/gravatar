9a7ee72c127c686190d688ce009468620469bf1d
Eugene Min <e.min@milax.com>
2016-05-28 16:29:27 +1100

    Update namespace

--- src/Gravatar.php
+++ src/Gravatar.php
@@ -1,36 +1,36 @@
 <?php
 
-namespace Milax;
+namespace MisterPaladin;
 
-class Gravatar {
-	
-	/**
-	 * Gravatar url
-	 * 
-	 * (default value: 'http://www.gravatar.com/avatar/')
-	 * 
-	 * @var string
-	 * @access protected
-	 * @static
-	 */
-	protected static $gravatarURL = 'http://www.gravatar.com/avatar/';
-	
-	/**
-	 * get function.
-	 * 
-	 * @access public
-	 * @static
-	 * @param string $email
-	 * @param int $size (default: null)
-	 * @return void
-	 */
-	public static function get($email, $size = null)
-	{
-		$url = self::$gravatarURL . md5($email);
-		if (!is_null($size))
-			$url .= '?s=' . $size;
-		
-		return $url;
-	}
-	
+class Gravatar
+{
+    /**
+     * Gravatar url
+     * 
+     * (default value: 'http://www.gravatar.com/avatar/')
+     * 
+     * @var string
+     * @access protected
+     * @static
+     */
+    protected static $gravatarURL = 'http://www.gravatar.com/avatar/';
+    
+    /**
+     * get function.
+     * 
+     * @access public
+     * @static
+     * @param string $email
+     * @param int $size (default: null)
+     * @return void
+     */
+    public static function get($email, $size = null)
+    {
+        $url = self::$gravatarURL . md5($email);
+        if (!is_null($size)) {
+            $url .= '?s=' . $size;
+        }
+        
+        return $url;
+    }
 }
