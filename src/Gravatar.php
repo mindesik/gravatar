6e888f8d623d0b182ad4750c7537263cd741c3af
Eugene Min <e.min@milax.com>
2016-06-05 16:06:54 +1100

    Fetch profiles

--- src/Gravatar.php
+++ src/Gravatar.php
@@ -2,35 +2,61 @@
 
 namespace MisterPaladin;
 
+use GuzzleHttp\Client;
+
 class Gravatar
 {
     /**
      * Gravatar url
      * 
-     * (default value: 'http://www.gravatar.com/avatar/')
+     * (default value: 'http://www.gravatar.com')
      * 
      * @var string
-     * @access protected
-     * @static
      */
-    protected static $gravatarURL = 'http://www.gravatar.com/avatar/';
+    protected static $gravatarURL = 'https://www.gravatar.com';
     
     /**
-     * get function.
+     * Deprecated function
      * 
-     * @access public
-     * @static
-     * @param string $email
-     * @param int $size (default: null)
-     * @return void
+     * @param  string $email
+     * @param  int $size
+     * @return string
      */
     public static function get($email, $size = null)
     {
-        $url = self::$gravatarURL . md5($email);
+        return self::avatar($email, $size);
+    }
+    
+    /**
+     * Get user avatar
+     * 
+     * @param  string $email
+     * @param  int $size
+     * @return string
+     */
+    public static function avatar($email, $size = null)
+    {
+        $url = sprintf('%s/avatar/%s', self::$gravatarURL, md5($email));
+        
         if (!is_null($size)) {
             $url .= '?s=' . $size;
         }
         
         return $url;
+    }
+    
+    /**
+     * Get user profile
+     * 
+     * @param  string $email
+     * @return stdClass
+     */
+    public static function profile($email)
+    {
+        $url = sprintf('%s/%s.json', self::$gravatarURL, md5($email));
+        
+        $client = new Client();
+        
+        return json_decode($client->get($url)->getBody()->getContents());
     }
 }
